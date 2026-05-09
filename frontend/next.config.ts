d4688f26ec29c9c2cff41ce520a81e4fcf469547
stejasav <tejasavsingh2517@gmail.com>
2026-05-09 16:47:36 +0530

    Fixed eslint issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,15 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
     ],
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
   },
 };
 
